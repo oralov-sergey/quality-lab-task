a4019ad2c3efc783ed4de1d6e7f1226d1675d9d6
SIBERIAN_WELLNESS <oralov-sergey@yandex.ru>
2021-08-18 12:49:45 +0300

    Added annotations @BeforeAll, @BeforeEach, @AfterEach, @AfterAll.

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,34 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class FirstTest {
+public class FirstTest extends SuperClass {
+
     @Test
-    void myTest(){
+    void myTest() {
         System.out.println("My first autotest running");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("Test №1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("Test №2");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("Test №3");
+    }
+
+    @Test
+    void test4() {
+        System.out.println("Test №4");
+    }
+
+    @Test
+    void test5() {
+        System.out.println("Test №5");
     }
 }
