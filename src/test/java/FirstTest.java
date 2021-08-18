99771a1f5718a797af8d7788a35803a9486452ff
SIBERIAN_WELLNESS <oralov-sergey@yandex.ru>
2021-08-18 16:57:57 +0300

    Asserts have been added to each method. AssertAll has been added to the last method.

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,34 +1,51 @@
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FirstTest extends SuperClass {
+    final int EXPECTED_NUMBER_IS_ONE = 1;
+    final int EXPECTED_NUMBER_IS_FOUR = 4;
+    final int EXPECTED_NUMBER_IS_FIVE = 5;
+    final int SUM = 2 + 2;
 
     @Test
     void myTest() {
         System.out.println("My first autotest running");
+        assertEquals(EXPECTED_NUMBER_IS_ONE, 1 / 0);
     }
 
     @Test
     void test1() {
         System.out.println("Test №1");
+        assertEquals(EXPECTED_NUMBER_IS_FOUR, SUM);
     }
 
     @Test
     void test2() {
         System.out.println("Test №2");
+        assertEquals(EXPECTED_NUMBER_IS_FIVE, SUM);
     }
 
     @Test
     void test3() {
         System.out.println("Test №3");
+        assertTrue(SUM == EXPECTED_NUMBER_IS_FOUR);
     }
 
     @Test
     void test4() {
         System.out.println("Test №4");
+        assertTrue(SUM == EXPECTED_NUMBER_IS_FIVE);
     }
 
     @Test
     void test5() {
         System.out.println("Test №5");
+        assertAll("comparisons",
+                () -> assertEquals(EXPECTED_NUMBER_IS_FOUR, SUM),
+                () -> assertEquals(EXPECTED_NUMBER_IS_FIVE, SUM),
+                () -> assertTrue(SUM == EXPECTED_NUMBER_IS_FOUR),
+                () -> assertTrue(SUM == EXPECTED_NUMBER_IS_FIVE)
+        );
     }
 }
