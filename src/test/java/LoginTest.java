e4e9686678c054697a8004db10f6d8ba8817d414
SIBERIAN_WELLNESS <oralov-sergey@yandex.ru>
2021-08-19 10:35:40 +0300

    The first UI test.

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,21 +1,48 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class LoginTest {
- WebDriver driver;
+    static WebDriver driver;
 
-    @Test
+    final String USER_FIELD_LOCATOR = "//input[@id='username']";
+    final String PASSWORD_FIELD_LOCATOR = "//input[@name='_password']";
+    final String LOG_IN_BUTTON_LOCATOR = "//input[@value='Войти']";
+    public final String MESSAGE_LOCATOR = "(//div[contains(.,'Invalid credentials.')])[8]";
+
+
+    @BeforeEach
     void initWebDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        new ChromeOptions().addArguments("--window-size=500,500");
-        driver.manage().window().setSize(new Dimension(200, 100));
-        driver.get("https://tt-develop.quality-lab.ru");
+        //    new ChromeOptions().addArguments("--window-size=500,500");
+        //    driver.manage().window().setSize(new Dimension(200, 100));
+        //   driver.get("https://tt-develop.quality-lab.ru");
         driver.manage().window().maximize();
-        driver.quit();
     }
+
+    @Test
+    void incorrectUserNameAndPassword() {
+        driver.navigate().to("https://tt-develop.quality-lab.ru");
+        driver.findElement(By.xpath(USER_FIELD_LOCATOR)).sendKeys("TestUser");
+        driver.findElement(By.xpath(PASSWORD_FIELD_LOCATOR)).sendKeys("Password");
+        assertThrows(NoSuchElementException.class, () -> {
+            driver.findElement(By.xpath(MESSAGE_LOCATOR));
+        });
+        driver.findElement(By.xpath(LOG_IN_BUTTON_LOCATOR)).click();
+
+    }
+
+    @AfterEach
+      void closeWebDriver() {
+        if (driver.findElement(By.xpath(MESSAGE_LOCATOR)) != null) {
+            driver.quit();
+        }
+    }
+
+
 }
